2e24f4d86f95355a31fd2768e85eb532b9f6c610
ShiWenting <1074635337@qq.com>
2025-06-05 11:33:11 +0800

    Improve scripts and add CLI

--- sync.js
+++ sync.js
@@ -1,14 +1,16 @@
-var child = require('child_process');
+const child = require('child_process');
+const path = require('path');
 const AK = "cdrub6Ompxht3mxMs-A5viisVFaepJwS1vWBcAT2";
 const SK = "XmnyG6niDI0QFHz_QN-BTkAB1eTKWMQ_KSdCfPfV";
 const Bucket = "v2gogo-transport";
 // 配置密钥
 // child.execSync(`qshell account "${AK}" "${SK}" ${Bucket}`);
 // 循环同步
-var timer = setInterval(function(){
+const timer = setInterval(function(){
 	console.log("sync!");
 	try {
-		child.execSync(`qshell qupload -c 5 localUploadConfig.json`);
+                const configPath = path.join(__dirname, 'localUploadConfig.json');
+                child.execSync(`qshell qupload -c 5 ${configPath}`);
 	}catch(e){
 		console.log(e);
 	}
